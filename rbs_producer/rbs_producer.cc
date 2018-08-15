1e699df3d7d5966b3feed178f27ac15159b8af53
Malte Wirkus <malte.wirkus@dfki.de>
2018-08-15 16:43:11 +0100

    bugfix in initialization of RigidBodyState data type in rbs_producer

--- rbs_producer/rbs_producer.cc
+++ rbs_producer/rbs_producer.cc
@@ -19,9 +19,11 @@ void rbs_producer_PI_trigger()
     if(i_sample < N_SAMPLES){
     	asn1SccBase_samples_RigidBodyState val;
         asn1SccBase_samples_RigidBodyState_Initialize(&val);
+        val.position.data.nCount = 3;
     	val.position.data.arr[0] = cos(STEP * i_sample);
     	val.position.data.arr[1] = sin(STEP * i_sample);
     	val.position.data.arr[2] = ((double)i_sample/N_SAMPLES);
+        val.orientation.im.nCount = 3;
     	val.orientation.im.arr[0] = cos(STEP * i_sample);
     	val.orientation.im.arr[1] = cos(0.5+STEP * i_sample);
     	val.orientation.im.arr[2] = cos(1.0+STEP * i_sample++);
